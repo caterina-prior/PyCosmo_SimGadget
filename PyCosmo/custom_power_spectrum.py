a1d8359dd68102db052d5150c4320ac535efdbc8
cprior <cprior@student.ethz.ch>
2025-03-29 08:41:01 +0100

    Added draft P(k) generation files
    To be deleted later!

---
+++ PyCosmo/custom_power_spectrum.py
@@ -0,0 +1,16 @@
+import PyCosmo
+import numpy as np
+
+# Comment out this line to remove the Watermark
+from Functions import pycosmowatermark
+
+import matplotlib
+import matplotlib.pyplot as plt
+from Functions.extra_functions import deltanorm
+
+
+# Import variables defined in input_parameters.param
+
+# PyCosmo instance
+cosmo = PyCosmo.build()
+
